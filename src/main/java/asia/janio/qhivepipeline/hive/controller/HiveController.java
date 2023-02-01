2fb4ad85799faeff0dbb5e7dae8fadebd06606c4
Tushar Mudgal <tushar.mudgal@janio.asia>
2023-02-01 13:37:13 +0530

    init

--- src/main/java/asia/janio/qhivepipeline/hive/controller/HiveController.java
+++ src/main/java/asia/janio/qhivepipeline/hive/controller/HiveController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/hive")
+@RestController
 public class HiveController {
     @Autowired
     private HiveServiceImpl hiveService;

7b9174ca2fd6fe3bb951e4a9ccda27ae728b0a95
Tushar Mudgal <tushar.mudgal@janio.asia>
2023-02-02 15:31:51 +0530

    init

--- src/main/java/asia/janio/qhivepipeline/QHivePipelineApplication.java
+++ src/main/java/asia/janio/qhivepipeline/QHivePipelineApplication.java
@@ -1,14 +1,26 @@
 package asia.janio.qhivepipeline;
 
+import asia.janio.qhivepipeline.hive.config.HiveConnection;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PreDestroy;
+
 @SpringBootApplication
+@Log4j2
 public class QHivePipelineApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(QHivePipelineApplication.class, args);
 //        HiveService.run();
+    }
+
+    @PreDestroy
+    public void onDestroy() throws Exception {
+        log.info("CLOSING HIVE CONNECTION...");
+        HiveConnection.getInstance().getConnection().close();
+        log.info("HIVE CONNECTION CLOSED");
     }
 
 }

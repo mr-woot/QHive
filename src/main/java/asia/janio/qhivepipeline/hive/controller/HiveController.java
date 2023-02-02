6d1c10e29849ab687a60459794cfcdf7e369bfd1
Tushar Mudgal <tushar.mudgal@janio.asia>
2023-02-02 13:06:28 +0530

    init

--- src/main/java/asia/janio/qhivepipeline/hive/controller/HiveController.java
+++ src/main/java/asia/janio/qhivepipeline/hive/controller/HiveController.java
@@ -1,12 +1,11 @@
 package asia.janio.qhivepipeline.hive.controller;
 
 import asia.janio.qhivepipeline.hive.service.HiveServiceImpl;
+import asia.janio.qhivepipeline.metadata.entity.CreateQueryPayload;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HiveController {
@@ -21,5 +20,11 @@ public class HiveController {
     @GetMapping("/describe-table")
     public ResponseEntity<?> describeTable(@RequestParam String tableName) {
         return ResponseEntity.status(HttpStatus.OK).body(hiveService.describeTable(tableName));
+    }
+
+    @PostMapping("executeHql")
+    public ResponseEntity<?> executeHql(@RequestBody CreateQueryPayload queryPayload) {
+        hiveService.select(queryPayload.getQuery());
+        return ResponseEntity.status(HttpStatus.CREATED).body(queryPayload);
     }
 }

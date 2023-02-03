15bcd5e8d91d56b9f550b8caf7bcc6a6430d000c
Tushar Mudgal <tushar.mudgal@janio.asia>
2023-02-03 13:30:27 +0530

    init

--- src/main/java/asia/janio/qhivepipeline/common/ApiResponse.java
+++ src/main/java/asia/janio/qhivepipeline/common/ApiResponse.java
@@ -1,9 +1,13 @@
 package asia.janio.qhivepipeline.common;
 
 import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
 @Builder
+@Getter
+@Setter
 public class ApiResponse {
     private String message;
     private String error;
